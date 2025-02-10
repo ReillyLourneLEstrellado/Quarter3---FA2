3ae30d803be494f2fd707ed05a32b625b1a150dc
ReillyLourneLEstrellado <reillylourne.estrellado@smc.pshs.edu.ph>
2025-02-10 21:04:54 +0800

    Create script.js
    Created functions with if statements

---
+++ script.js
@@ -0,0 +1,47 @@
+body {
+    font-family: "Courier New", monospace;
+    text-align: center;
+    margin: 50px;
+    color: blue;
+}
+p2 {
+    font-family: "Courier New", monospace;
+    text-align: center;
+    margin: 50px;
+    color: green;
+}
+
+
+.container {
+    max-width: 400px;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid red;
+    border-radius: 10px;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+    background-color: beige;
+}
+
+input {
+    width: 90%;
+    padding: 10px;
+    margin: 10px 0;
+    border: 1px solid blue;
+    background-color: white;
+    color: red;
+}
+
+button {
+    padding: 10px 20px;
+    margin: 5px;
+    cursor: pointer;
+    color: blue;
+    border: 1px solid black; 
+    background-color: white;
+}
+
+
+button:hover {
+    background-color: yellow;
+    color: red;
+}
